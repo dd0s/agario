54ecbcb162e198260ba54a29c9cf30baa02619ec
dd0s <pvvner@yandex.ru>
2017-10-10 02:19:08 +0300

    Make world move around player. Make player eat other blobs and get bigger

--- blob.js
+++ blob.js
@@ -2,6 +2,22 @@ function Blob(x, y, radius) {
     this.pos = createVector(x, y);
     this.r = radius;
 
+    this.update = function () {
+        var vel = createVector(mouseX - width/2, mouseY - height/2);
+        vel.setMag(3);
+        this.pos.add(vel);
+    }
+
+    this.eats = function (other) {
+        var d = p5.Vector.dist(this.pos, other.pos);
+        if (d < this.r + other.r) {
+            this.r += other.r;
+            return true;
+        } else {
+            return false;
+        }
+    }
+    
     this.show = function () {
         fill(255);
         ellipse(this.pos.x, this.pos.y, this.r * 2, this.r * 2);
--- sketch.js
+++ sketch.js
@@ -6,16 +6,25 @@ function setup () {
     createCanvas(600, 600);
     blob = new Blob(width/2, height/2, 64);
 
-    for (var i = 0; i < 10; i++) {
-        blobs[i] = new Blob(random(width), random(height), 16);
+    for (var i = 0; i < 15; i++) {
+        var x = random(-width, width*2);
+        var y = random(-height, height*2);
+        blobs[i] = new Blob(x, y, 16);
     }
 }
 
 function draw () {
     background(0);
-    blob.show();
-    for (var i = 0; i < 10; i++) {
+    translate(width/2 - blob.pos.x, height/2 - blob.pos.y);
+
+    for (var i = blobs.length - 1; i >= 0; i--) {
         blobs[i].show();
+        if (blob.eats(blobs[i])) {
+            blobs.splice(i, 1);
+        }
     }
+
+    blob.show();
+    blob.update();
 }
 
